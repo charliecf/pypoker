e1ddd77b00e75b478f763ee40c58dd423a2546fb
charliecf <charlie@debian>
2016-10-08 11:07:44 -0400

    initial file

---
+++ poker-simulator.py
@@ -0,0 +1,16 @@
+"""
+Pot Odds
+- Pot / Your Contributions
+Equity (chance to win)
+- Flop: a/47 + ((47-a)/47 * a/46)
+- Turn: a/46
+= 1-A / A
+"""
+
+Object = Deck of Cards
+
+Dealer Object to deal X cards
+
+Simulator Mode (Texas Hold'em can be one variant of the game, while simulation is simply another variation of a game master who sets the pace of the game)
+
+
